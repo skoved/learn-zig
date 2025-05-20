b0ffffbe0841459316eecb23dc08cde065c650c2
skoved <sam.koved@gmail.com>
2025-05-19 22:52:09 -0400

    how do imports work?

--- part1/learning.zig
+++ part1/learning.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const useri = @import("models/user.zig");
+const User = useri.User;
+const MAX_POWER = useri.MAX_POWER;
+
 // This code won't compile if `main` isn't `pub` (public)
 pub fn main() void {
     const user = User{
@@ -9,8 +13,3 @@ pub fn main() void {
 
     std.debug.print("{s}'s power is {d}\n", .{user.name, user.power});
 }
-
-pub const User = struct {
-    power: u64,
-    name: []const u8,
-};
---
+++ part1/models/user.zig
@@ -0,0 +1,6 @@
+pub const MAX_POWER = 100_000;
+
+pub const User = struct {
+    power: u64,
+    name: []const u8,
+};
